66e75c2e6a052cd6b673bdeccbdbe8e6d87d218d
Thomas Peterson <62221992+pacyao13@users.noreply.github.com>
2021-02-23 20:44:42 -0600

    Update client.py
    client now sends an entire HTTP request message

--- client.py
+++ client.py
@@ -1,17 +1,34 @@
 import socket
 import time
 
+hostName = socket.gethostname()
+hostIP = socket.gethostbyname(hostName)
+
+
+
+
 # 1. create a socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # 2. connect to remote server
-client_socket.connect(('192.168.1.9',50510))
+client_socket.connect(('10.200.3.221', 50001))
 
-# 3. Ask user to enter a message
-message = input('Enter a message you want to send:')
+# 3. Have client create HTTP request
+method = input('Method: ')
+url = input('URL: ')
+version = input('Version: ')
+host = input('Host: ')
+connection = input('Connection: ')
+
+entity_body = input('Entity body: ')
+
+req = str(method)+' '+str(url)+' '+str(version)+'\n'+"Host: "+host+'\n'+"Connection: "+connection+'\n\n'+entity_body
+
+
 
 # 4. send that message to the server
-client_socket.send(message.encode('utf-8'))
+client_socket.send((hostName + ": " +'\n'+req).encode('utf-8'))
 time.sleep(2)
+
 # 5. Close the socket
 client_socket.close()
